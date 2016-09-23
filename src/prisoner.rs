947ce2852d904fbfe1f14796ac2b5070cd9da4d1
Julian Laubstein <julian.laubstein@9elements.com>
2016-09-23 19:36:40 +0200

    Documented prisoner.rs

--- src/prisoner.rs
+++ src/prisoner.rs
@@ -1,32 +1,53 @@
-use std::sync::mpsc::Sender;
-use std::collections::HashMap;
-use guard::Guard;
 use complaint::Complaint;
+use guard::Guard;
+use std::collections::HashMap;
+use std::sync::mpsc::Sender;
 
 
 
+/// A simple type definition for consistency
 pub type PrisonerVec = Vec<Prisoner>;
 
 pub struct Prisoner {
+    /// The name of the prisoner
     pub name: String,
+    /// A hashmap mapping a need to the respective sender object of the guard
     guard_map: HashMap< String, Sender< Complaint > >,
 }
 
 impl Prisoner {
+    /// Sends a complaint to the respective guard
     pub fn complain(&self, complaint: Complaint) {
         self.guard_map[&complaint.need].send(complaint).unwrap();
     }
 
+    /// Adds a guard to the guard map
     pub fn add_guard(&mut self, guard: &Guard) {
         self.guard_map.insert(guard.need.clone(), guard.get_complaint_sender());
     }
 
+    /// Constructs a new `PrisonerVec`
+    ///
+    /// # Examples
+    ///
+    /// let name_vector = vec![
+    ///     "Alfred",
+    ///     "Jim",
+    ///     "Henry",
+    /// ];
+    ///
+    /// let prisoner_vector = Prisoner::new_vec(name_vec);
     pub fn new_vec(name_vec: Vec<&str>) -> PrisonerVec {
         name_vec.into_iter().map(|name| {
             Prisoner::new(name)
         }).collect()
     }
 
+    /// Constructs a new `Prisoner`
+    ///
+    /// # Examples
+    ///
+    /// let prisoner = Prisoner::new("Jacob");
     pub fn new(name: &str) -> Prisoner {
         Prisoner {
             name: name.to_string(),
