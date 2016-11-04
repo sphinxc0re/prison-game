e9c1817b682f73858f3201887b395d9fdb084a13
Julian Laubstein <julianlaubstein@yahoo.de>
2016-11-04 14:21:47 +0100

    Added interaction between guard and prisoner (#2)

--- src/prisoner.rs
+++ src/prisoner.rs
@@ -1,7 +1,10 @@
-use complaint::Complaint;
+use message::Message;
 use guard::Guard;
 use std::collections::HashMap;
 use std::sync::mpsc::Sender;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::channel;
+
 
 
 
@@ -12,18 +15,28 @@ pub struct Prisoner {
     /// The name of the prisoner
     pub name: String,
     /// A hashmap mapping a need to the respective sender object of the guard
-    guard_map: HashMap< String, Sender< Complaint > >,
+    guard_map: HashMap<String, Sender<Message>>,
+    /// The sender of the channel, typed
+    sender: Sender<Message>,
+    /// The receiver of the channel, typed
+    receiver: Receiver<Message>,
 }
 
 impl Prisoner {
     /// Sends a complaint to the respective guard
-    pub fn complain(&self, complaint: Complaint) {
-        self.guard_map[&complaint.need].send(complaint).unwrap();
+    pub fn complain(&self, complaint: Message) {
+        match complaint {
+            Message::Complain(ref need, ref ammount, ref prisoner_name, ref sender) => {
+                let actual_complaint = Message::Complain(need.clone(), ammount.clone(), prisoner_name.clone(), sender.clone());
+                self.guard_map[need].send(actual_complaint).unwrap();
+            },
+            other => panic!("Prisoner is unable to send message of type: {:?}", other)
+        }
     }
 
     /// Adds a guard to the guard map
     pub fn add_guard(&mut self, guard: &Guard) {
-        self.guard_map.insert(guard.need.clone(), guard.get_complaint_sender());
+        self.guard_map.insert(guard.need.clone(), guard.get_sender());
     }
 
     /// Constructs a new `PrisonerVec`
@@ -43,15 +56,27 @@ impl Prisoner {
         }).collect()
     }
 
+    pub fn wait_for_and_receive_message(&self) -> Option<Message> {
+        self.receiver.recv().ok()
+    }
+
+    /// Returns a copy of the message sender
+    pub fn get_sender(&self) -> Sender<Message> {
+        self.sender.clone()
+    }
+
     /// Constructs a new `Prisoner`
     ///
     /// # Examples
     ///
     /// let prisoner = Prisoner::new("Jacob");
     pub fn new(name: &str) -> Prisoner {
+        let (snd, rec) = channel();
         Prisoner {
             name: name.to_string(),
-            guard_map: HashMap::new()
+            guard_map: HashMap::new(),
+            sender: snd,
+            receiver: rec
         }
     }
 }
